3eb143de208e7527167d8c4ccf83d42439bac13b
Odwa <odmafundityala@gmail.com>
2025-06-19 23:46:16 +0200

    done

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 
-
 let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
-let apiKey = checkApiKey();
+let apiKey = localStorage.getItem("apiKey"); // Just get the key without redirect
 
+// function to check API key when needed
 function checkApiKey() {
-    if (!localStorage.getItem("apiKey")) {
+    if (!apiKey) {
         window.open("enter-api-key.html", "_self");
     }
-    return localStorage.getItem("apiKey");
 }
